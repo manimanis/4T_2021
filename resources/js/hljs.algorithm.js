d937916fa32af182bc39ab2979a75fc37d4d1a78
Mohamed Anis MANI <manimohamed@gmail.com>
2021-12-04 14:20:10 +0100

    mise à jour chapitre 3

--- resources/js/hljs.algorithm.js
+++ resources/js/hljs.algorithm.js
@@ -3,10 +3,11 @@ hljs.registerLanguage('algorithm', (hljs) => {
     name: 'algorithm',
     case_insensitive: true, 
     aliases: ['alg'],
-    lexemes : '[\\\'àéèêâäöüÄÖÜßa-zA-Z]+',
+    // lexemes : '[\\\'àéèêâäöüÄÖÜßa-zA-Z]+',
     keywords: {
       keyword: 'algorithme retourner programme fonction procédure début ecrire écrire afficher lire si alors sinon fin tantque faire répéter jusqu\'à mod div pour de à pas non et ou ouex',
-      literal: 'faux vrai nul'
+      literal: 'faux vrai nul',
+      $pattern: '[\\\'àéèêâäöüÄÖÜßa-zA-Z]+',
     },
     contains: [
       {
